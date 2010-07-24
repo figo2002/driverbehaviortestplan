e99c4738beec0ce615f3177836d540406e99fa98
figo2004 <figo2004@967d4ed6-f6b8-11de-b622-b70f56c379d3>
2010-07-24 08:34:04 +0000

    back cover added; a3cover fixed for winxp

--- seuthesis-2.0.0/a3cover/a3cover.bat
+++ seuthesis-2.0.0/a3cover/a3cover.bat
@@ -3,7 +3,8 @@ pdflatex -interaction=nonstopmode bookspine_ver.tex
 pdfcrop --margins 2 --clip bookspine_ver.pdf bookspine_res.pdf
 pdflatex -interaction=nonstopmode A3cover.tex
 
-rm *.aux *.log
+del *.aux *.log
 
-rm bookspine_hor.pdf
-rm bookspine_ver.pdf
+del bookspine_hor.pdf
+del bookspine_ver.pdf
+del bookspine_res.pdf
